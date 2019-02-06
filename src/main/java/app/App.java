926c4580d67a23d95f73a4468def1a745327aeb1
Michael Gagliardo <mgagliardo91@gmail.com>
2019-02-06 13:55:30 -0500

    MMM-1223

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -27,5 +27,6 @@ public class App
         System.out.println( "Hello World, Mike Again Again!" );
         System.out.println( "Hello World, Mike Again Again!" );
         System.out.println( "Hello World, Mike Again Again!" );
+        System.out.println( "Hello World, Mike Again Again!" );
     }
 }
